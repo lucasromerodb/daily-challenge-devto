6afd8c47f2d4dc8dbc5cb4b6f07ab75e7334aef3
Luke <lucasromerodb@gmail.com>
2020-04-06 21:51:09 -0300

    001 String peeler

---
+++ 001-string-peeler.js
@@ -0,0 +1,20 @@
+/**
+ * Day 001
+ * String Peeler
+ *
+ * Your goal is to create a function that removes the first and last letters of a string.
+ * Strings with two characters or less are considered invalid.
+ * You can choose to have your function return null or simply ignore.
+ *
+ * https://dev.to/thepracticaldev/daily-challenge-1-string-peeler-4nep
+ */
+
+const peelString = (str = "Hello world") => {
+  if (str.length > 2 && str !== "") {
+    str = str.slice(1, str.length - 1);
+  }
+
+  return str;
+};
+
+console.log(peelString());
